5884c5100af14c38ce89dad2f5994dd0940981ae
Pedro Antonio <pedro_arxp@hotmail.com>
2023-08-18 09:30:29 -0300

    initializing a new project angular and create a new component board

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BoardComponent } from './board/board.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'tic-tac-toe', component: BoardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
